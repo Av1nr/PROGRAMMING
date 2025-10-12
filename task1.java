a101820dfc22726c67b7ed3d850f4d2883b6a840
Av1nr <askarsteam5501@gmail.com>
2025-10-13 00:02:51 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -3,15 +3,22 @@ public class task1{
     public static void main(String[] args){
         Scanner enter = new Scanner(System.in);
         String slovo = enter.nextLine();
-        slovo = slovo.replaceAll(" ", "");
-        slovo = slovo.toLowerCase();
-        boolean flag = false;
+        String newslovo = "";
         for (int i = 0 ; i < slovo.length() ; i++){
-            if (slovo.charAt(i) == slovo.charAt(slovo.length() - 1 - i)){
+            char bukva = slovo.charAt(i);
+            if (bukva != ' ' && bukva != '.' && bukva != ',' && bukva != ':'){
+                newslovo = slovo.charAt(i) + newslovo;
+            }
+        }
+        newslovo = newslovo.toLowerCase();
+        boolean flag = false;
+        for (int i = 0 ; i < newslovo.length() ; i++){
+            if (newslovo.charAt(i) == newslovo.charAt(newslovo.length() - 1 - i)){
                 flag = true;
             }
         }
         enter.close();
+        System.out.println(newslovo);
         System.out.println(flag);
     }
 }
