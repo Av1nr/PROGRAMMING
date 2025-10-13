724421b99e608bf347b017771e34a6b192f4be2e
Av1nr <askarsteam5501@gmail.com>
2025-10-13 13:54:59 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -10,10 +10,9 @@ public class task1{
                 newslovo = slovo.charAt(i) + newslovo;
             }
         }
-        newslovo = newslovo.toLowerCase();
         boolean flag = false;
         for (int i = 0 ; i < newslovo.length() ; i++){
-            if (newslovo.charAt(i) == newslovo.charAt(newslovo.length() - 1 - i)){
+            if (ToLower(newslovo.charAt(i)) == ToLower(newslovo.charAt(newslovo.length() - 1 - i))){
                 flag = true;
             }
             else{
@@ -24,5 +23,14 @@ public class task1{
         System.out.println(newslovo);
         System.out.println(flag);
     }
+    public static char ToLower(char x){
+        if ('A' <= x && x >= 'Z'){
+            int y = x;
+            y = y + 32;
+            return (char) y;
+        }
+        else{
+            return x;
+        }
+    }
 }
-
