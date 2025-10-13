6e19981ff8c3b22bcf46ca723aad61893d84a733
Av1nr <askarsteam5501@gmail.com>
2025-10-13 16:20:40 +0300

    Add files via upload

---
+++ task3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class task3{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String predl = scanner.nextLine().trim();
+        String bukvi = "уеёыаоэяию";
+        int c = 0;
+        int fin = 0;
+        for (int i = 0; i < predl.length(); i++){
+            if (bukvi.contains(predl.charAt(i) + "")){
+                fin++;
+            }
+        }
+        if(fin == 3){
+            System.out.println(true);
+        } else{
+            System.out.println(false);
+        }
+    }
+}
